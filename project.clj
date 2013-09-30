fd43a57409b741e0c74c9da927e3dc4bf98f6a3b
Peter Tillemans <pti@snamellit.com>
2013-09-30 03:05:06 +0200

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,8 +1,14 @@
-(defproject casproxy "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject casproxy "0.1.1-SNAPSHOT"
+  :description "Connect transparently to a CAS protected server through a local server."
+  :url "https://github.com/ptillemans/casproxy"
+  :license {:name "GNU Public License"
+            :url "http://www.gnu.org/licenses/gpl-2.0.html"}
+  :deploy-repositories [["snapshots"
+                         {:url "http://nexus.colo.elex.be:8081/nexus/content/repositories/snapshots"
+                          :creds :gpg}]
+                        ["releases"
+                         {:url "http://nexus.colo.elex.be:8081/nexus/content/repositories/releases"
+                          :creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.7"]
                  [ring/ring-core "1.2.0"]
