53616dfcc10f22a49b1c5b3636016c9e90965fd0
Peter Tillemans <pti@snamellit.com>
2017-01-20 16:19:17 +0100

    Version 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject casproxy "0.1.4"
+(defproject casproxy "0.1.5-SNAPSHOT"
   :description "Connect transparently to a CAS protected server through a local server."
   :url "https://github.com/ptillemans/casproxy"
   :license {:name "GNU Public License"
